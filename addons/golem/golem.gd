56b7b4f8623ad9ddc3e12450a4917b66a32fa982
Azure Su <qqsu.azure@gmail.com>
2026-05-08 16:53:57 +0800

    better way to init golem

--- addons/golem/golem.gd
+++ addons/golem/golem.gd
@@ -1,8 +1,6 @@
 @tool
 class_name Golem extends CharacterBody3D
 
-@export var skeleton: Skeleton3D
-
 @onready var animation_state_machine: AnimationStateMachine = $AnimationStateMachine
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
@@ -20,14 +18,28 @@ class_name Golem extends CharacterBody3D
 var height_ratio: float:
 	get: return height / height_baseline
 
+var skeleton: Skeleton3D:
+	get:
+		if not skeleton:
+			skeleton = _look_for_skeleton($Model)
+		return skeleton
+
 func _ready() -> void:
 	if skeleton:
 		animation_player.root_node = skeleton.get_path()
-	else:
-		if not Engine.is_editor_hint():
-			Glaze.log_error("No skeleton found in golem: %s", self)
+	elif not Engine.is_editor_hint():
+		Glaze.log_error("No skeleton found in golem: %s", self)
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	move_and_slide()
+
+func _look_for_skeleton(node: Node) -> Skeleton3D:
+	if node is Skeleton3D:
+		return node
+	for ch in node.get_children():
+		var s:= _look_for_skeleton(ch)
+		if s:
+			return s
+	return null
